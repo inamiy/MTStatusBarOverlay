2b82fbb410e1326153637d42de6304e2e3f2de81
Yasuhiro Inami <inamiy@gmail.com>
2013-01-29 19:16:59 +0900

    Add MT_START_TIME & MT_STOP_TIME (for debugging)

--- MTStatusBarOverlay+TimeTracking.h
+++ MTStatusBarOverlay+TimeTracking.h
@@ -10,6 +10,14 @@
 
 @interface MTStatusBarOverlay (TimeTracking)
 
+#if defined(DEBUG) || defined(MT_DEBUG)
+#   define MT_START_TIME(name)  [MTStatusBarOverlay startTimeTrackingWithName:name]
+#   define MT_STOP_TIME(name)   [MTStatusBarOverlay stopTimeTrackingWithName:name]
+#else
+#   define MT_START_TIME(name)  ;
+#   define MT_STOP_TIME(name)   ;
+#endif
+
 - (void)startTimeTrackingWithName:(NSString*)name;
 - (void)stopTimeTrackingWithName:(NSString*)name;
 
